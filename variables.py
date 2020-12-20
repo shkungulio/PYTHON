47ad622347dce57f9c62b23b8cd7c6b2b7135d6d
Seif Kungulio <shkungulio@gmail.com>
2020-12-19 22:25:37 -0600

    Modified the variables.py file
    Added more examples to demonstarte the use of variables in python!

--- variables.py
+++ variables.py
@@ -3,3 +3,11 @@
 color = "Red"
 thing = "Danger"
 print(color + " is the color of " + thing)
+
+color = "Yellow"
+thing = "Warning"
+print(color + " is the color of " + thing)
+
+color = "Green"
+thing = "Safety"
+print(color + " is the color of " + thing)
