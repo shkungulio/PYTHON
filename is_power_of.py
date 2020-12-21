abc461b22252242e63d77885d25ff2870fa7619d
Seif Kungulio <shkungulio@gmail.com>
2020-12-21 17:46:35 -0600

    The function returns whether the number is a power of the given base.
    The is_power_of function return whether the number is a power of the given base.
    Note: base is assumed to be a positive number. Tip: for functions that return
    a boolean value, you can return the result of a comparison.

---
+++ is_power_of.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#The is_power_of function return whether the number is a power of the given base.
+#Note: base is assumed to be a positive number. Tip: for functions that return
+#a boolean value, you can return the result of a comparison.
+
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return number == 1
+  result = number//base
+
+  # Recursive case: keep dividing number by base.
+  return is_power_of(result, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
