c8c959b441ad10ad0ca197f703f75021a2ce7811
Seif Kungulio <shkungulio@gmail.com>
2020-12-21 15:50:51 -0600

    A script that prints the multiples of 7 between 0 and 100.
    A script that prints the multiples of 7 between 0 and 100. Print one multiple
    per line and avoid printing any numbers that aren't multiples of 7.
    Remember that 0 is also a multiple of 7.

---
+++ multiple_of_number.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#A script that prints the multiples of 7 between 0 and 100. Print one multiple
+#per line and avoid printing any numbers that aren't multiples of 7.
+#Remember that 0 is also a multiple of 7.
+
+for x in range(0, 100, 7):
+    print(x)
