6c27576c0dffc5d0785a22572994052f55f22b31
Seif Kungulio <shkungulio@gmail.com>
2020-12-23 18:06:21 -0600

    The function is supposed to compare just the alphanumeric content of two strings.
    The compare_strings function is supposed to compare just the alphanumeric
    content of two strings, ignoring upper vs lower case and punctuation.
    But something is not working. Fill in the code to try to
    find the problems, then fix the problems.

--- compare_strings.py
+++ compare_strings.py
@@ -6,20 +6,20 @@
 
 import re
 def compare_strings(string1, string2):
-  #Convert both strings to lowercase
-  #and remove leading and trailing blanks
-  string1 = string1.lower().strip()
-  string2 = string2.lower().strip()
+    #Convert both strings to lowercase
+    #and remove leading and trailing blanks
+    string1 = string1.lower().strip()
+    string2 = string2.lower().strip()
 
-  #Ignore punctuation
-  punctuation = r"[.?!,;:\-']"
-  string1 = re.sub(punctuation, r"", string1)
-  string2 = re.sub(punctuation, r"", string2)
+    #Ignore punctuation
+    punctuation = r"[.?!,;:\-']"
+    string1 = re.sub(punctuation, r"", string1)
+    string2 = re.sub(punctuation, r"", string2)
 
-  #DEBUG CODE GOES HERE
-  print(string1, "==", string2)
+    #DEBUG CODE GOES HERE
+    print(string1, "==", string2)
 
-  return string1 == string2
+    return string1 == string2
 
 print(compare_strings("Have a Great Day!", "Have a great day?")) # True
 print(compare_strings("It's raining again.", "its raining, again")) # True
